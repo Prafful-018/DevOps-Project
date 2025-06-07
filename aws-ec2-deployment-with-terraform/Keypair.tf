acfac58367bc1f5eedb4898b84cd6ed08d09f0af
Prafful Mishra <128427528+Prafful-018@users.noreply.github.com>
2025-06-07 12:39:14 +0530

    Update Keypair.tf

--- aws-ec2-deployment-with-terraform/Keypair.tf
+++ aws-ec2-deployment-with-terraform/Keypair.tf
@@ -1,4 +1,4 @@
 resource "aws_key_pair" "dove-key" {
   key_name   = "dove-key"
-  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHt06XI6WAdOkTEeUqkqpu9op2QPb+41GMw5C604rTJp Admin@LAPTOP-6G0SMMEG"
+  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHt06XI6WAdOkTEeUqkqpu9op2QPb+41GMw5C604rTJp6G0SMMEG"
 }
